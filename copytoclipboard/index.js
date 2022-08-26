2dc16dec6d122ef2d28ee23d751a29bc78915fe4
Tushar Jain <tusharjain.752001@gmail.com>
2022-08-26 19:59:53 +0530

    Create index.js

---
+++ copytoclipboard/index.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    let text=document.getElementById('text');
+    text.selected();
+    text.setSelectionRange(0,1000); //it takes two parameters count of the starting alphabet and the ending
+
+}
