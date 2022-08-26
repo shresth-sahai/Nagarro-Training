4fe2fdc5982a00446ba3bc27cc8b5451820c6a59
Tushar Jain <tusharjain.752001@gmail.com>
2022-08-26 19:20:40 +0530

    Add files via upload

---
+++ cricket/index.js
@@ -0,0 +1,45 @@
+const r = document.querySelector('#render');
+const url = "https://api.cricapi.com/v1/currentMatches?apikey=c98cc071-432a-44a9-9aac-78e6d084ebdb&offset=0";
+const btn = document.querySelector('.mybtn');
+let loading = false;
+btn.addEventListener('click', () => {
+    r.innerHTML = `<h5 class="text-center my-2" style="width:100%">Loading...</h5>`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const match = data.data;
+            if (!match) return;
+            r.innerHTML = '';
+            match.forEach(item => {
+                r.innerHTML += `<div class="col-sm-12 col-md-6">
+<div class="card mx-auto rounded my-4">
+    <div class="card-header text-white">${item.name}</div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-6">
+                <div class="d-flex justify-content-start align-items-center">
+                    <img src=${item.teamInfo[0].img} class="card-img-top img" alt="team">
+                    <span>${item.teamInfo[0].shortname}</span>
+                </div>
+                    <p>Runs : ${item.score[0].r}</p>
+                    <p>Wickets : ${item.score[0].w}</p>
+                    <p>Over : ${item.score[0].o}</p>
+            </div>
+            <div class="col-6">
+                <div class="d-flex justify-content-start align-items-center">
+                    <img src=${item.teamInfo[1].img} class="card-img-top img" alt="team">
+                    <span>${item.teamInfo[1].shortname}</span>
+                </div>
+                <p>Runs : ${item.score[1].r}</p>
+                <p>Wickets : ${item.score[1].w}</p>
+                <p>Over : ${item.score[1].o}</p>
+            </div>
+            </div>
+            <p class="py-2 my-2 status" >${item.status}</p>
+    </div>
+</div>
+</div>`
+            });
+        })
+})
+
